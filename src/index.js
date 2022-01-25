d58955758154dcabd7893bc481198f753ee3b138
ceelosh <mmunizdaluz@gmail.com>
2022-01-24 22:16:13 -0300

    chore: add undone command

--- src/index.js
+++ src/index.js
@@ -6,6 +6,9 @@ const version = require("../package.json").version;
 // commands
 const { add } =  require("./commands/add");
 const { list } =  require("./commands/list");
+const { done } = require("./commands/done");
+const { undone } = require("./commands/undone");
+
 
 // get version from package
 program.version(version);
@@ -19,5 +22,15 @@ program
 	.command("list")
 	.description("Lista todos os items na lista de to-do.")
 	.action(() => list());
+
+program
+  .command("done [id]")
+  .description("Marca um item como finalizado.")
+  .action(async (itemId) => await done(itemId));
+
+program
+  .command("undone [id]")
+  .description("Marca um item como não-finalizado")
+  .action(async (itemId) => await undone(itemId));
 
 program.parse(process.argv);
